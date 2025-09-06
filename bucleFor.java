4f1e2154040cd046396971f4b56736c02039ed8c
wilfreider trochez <wilfreidertrochez99@gmail.com>
2025-09-05 22:13:48 -0500

    se agrega el proyecto

---
+++ bucleFor.java
@@ -0,0 +1,11 @@
+public class bucleFor {
+
+    public static void main (String[] args ) {
+
+        for (int cont = 0; cont <= 10; cont++) {
+            System.out.println("estoy dando vueltas con for, estoy en la vuelta " + cont);
+        }
+    }
+
+}
+    
