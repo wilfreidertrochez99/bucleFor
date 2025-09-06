3fc62797684cc6cf369d39d3364cc49e3fe192ea
wilfreider trochez <wilfreidertrochez99@gmail.com>
2025-09-05 22:37:45 -0500

    se grega este mensaje al main

--- bucleFor.java
+++ bucleFor.java
@@ -5,6 +5,8 @@ public class bucleFor {
         for (int cont = 0; cont <= 10; cont++) {
             System.out.println("estoy dando vueltas con for, estoy en la vuelta " + cont);
         }
+
+        System.out.println("este es un nuevo mensaje");
     }
 
 }
